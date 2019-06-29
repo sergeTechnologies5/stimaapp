593f9a55e649dfef4c8496e7702a9884d8a488cb
njerunjeru <njesh74@gmail.com>
2019-06-29 16:47:31 +0300

    DD

--- app/camera.py
+++ app/camera.py
@@ -11,7 +11,7 @@ def randomString(stringLength=10):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
- def thread_function_startCam(target=startCam()):
+def thread_function_startCam(target=startCam()):
 
     startCam()
 def startCam():
