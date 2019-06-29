311c177c027fbddd2e4f74a32ffd22cc6029f1f6
njerunjeru <njesh74@gmail.com>
2019-06-29 14:51:44 +0300

    pi camera

--- app/camera.py
+++ app/camera.py
@@ -1,60 +1,17 @@
-import cv2 
-import time 
 import random
 import string
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
 key = cv2. waitKey(1)
-
-
-essing the Raspberry Pi Camera with OpenCV and PythonPython
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
-import cv2
-
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-
-# allow the camera to warmup
-time.sleep(0.1)
-
-# grab an image from the camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
-
-# display the image on screen and wait for a keypress
-cv2.imshow("Image", image)
-cv2.waitKey(0)
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
-import cv2
- 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-
 
 def randomString(stringLength=10):
     """Generate a random string of fixed length """
@@ -64,6 +21,7 @@ def randomString(stringLength=10):
 def startCam():
     while True:
         rawCapture = PiRGBArray(camera)
+ 
         # grab an image from the camera
         camera.capture(rawCapture, format="bgr")
         frame = rawCapture.array
