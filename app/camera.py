a30eba7e14198bb3f3d921c9daad5587372a94a5
njerunjeru <njesh74@gmail.com>
2019-06-29 14:49:10 +0300

    pi camera

--- app/camera.py
+++ app/camera.py
@@ -3,7 +3,58 @@ import time
 import random
 import string
 key = cv2. waitKey(1)
-webcam = cv2.VideoCapture(1)
+
+
+essing the Raspberry Pi Camera with OpenCV and PythonPython
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+
+# display the image on screen and wait for a keypress
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+ 
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+
 
 def randomString(stringLength=10):
     """Generate a random string of fixed length """
@@ -12,7 +63,10 @@ def randomString(stringLength=10):
     
 def startCam():
     while True:
-        check, frame = webcam.read()
+        rawCapture = PiRGBArray(camera)
+        # grab an image from the camera
+        camera.capture(rawCapture, format="bgr")
+        frame = rawCapture.array
         key = cv2.waitKey(1)
         image_url = randomString(5)+'.jpg'
         filepath = 'static/img/camera/'+image_url
