ec8495a7d0bef5511360a713214dd1b39cfb17c4
njerunjeru <njesh74@gmail.com>
2019-06-29 16:23:24 +0300

    cr

--- app/camera.py
+++ app/camera.py
@@ -2,7 +2,7 @@ import random
 import string
 import time
 from picamera import PiCamera
-
+from ocr import getOCR
 camera = PiCamera()
 camera.rotation = 180
 camera.start_preview(alpha = 200)
@@ -10,13 +10,17 @@ def randomString(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
-    
+
 def startCam():
     while True:
         imageName = randomString(stringLength=5)+'.jpg'
         imagePath = 'static/img/camera/' + imageName
         camera.capture(imagePath)
-        print("Image saved!")
+
+        value = getOCR(imagePath)
+        if value == None:
+            value = getOCR()
+        print("value  saved is = " + value)
         time.sleep(10)
     camera.stop_preview()
 
--- app/ocr.py
+++ app/ocr.py
@@ -1,14 +1,9 @@
 import cv2
 import sys
 import pytesseract
- 
-if __name__ == '__main__':
-
-    if len(sys.argv) < 2:
-        print('Usage: python ocr_simple.py image.jpg')
-        sys.exit(1)
+def getOCR(value = 'static/img/camera/default.jpg'):
     # Read image path from command line
-    imPath = sys.argv[1]
+    imPath = value
 
     # Uncomment the line below to provide path to tesseract manually
     pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
@@ -17,12 +12,12 @@ if __name__ == '__main__':
     # '-l eng'  for using the English language
     # '--oem 1' for using LSTM OCR Engine
     config = ('-l eng --oem 1 --psm 3')
-    
     # Read image from disk
     im = cv2.imread(imPath, cv2.IMREAD_COLOR)
     
     # Run tesseract OCR on image
     text = pytesseract.image_to_string(im, config=config)
-    
     # Print recognized text
-    print(text)
+    return text
+if __name__ == '__main__':
+    print(getOCR())
